9bf2a0d97695afd5c5c61e2bfa883ce7c4eec60b
Liam Naddell <liamnprg@gmail.com>
2022-11-13 17:03:06 -0500

    probably last commit

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,21 @@ module Main where
 
 f (x,y) = x*exp(-1*x*y)
 
+d :: Integer -> Double
+d a = fromIntegral a
+
 -- change indices to be integers
-type  Cij = (Double -> Double -> Integer -> Double -> Double -> (Double, Double))
+type  Cij = (Double -> Double -> Integer -> Integer -> Integer -> (Double, Double))
 
 bottom_left :: Cij
-bottom_left a b n i j = (dx*(i - 1.0),dy*(j-1.0))
-    where dx = (b-a)/(fromIntegral n)
-          dy = (b-a)/(fromIntegral n)
+bottom_left a b n i j = (dx*(d i - 1.0),dy*(d j-1.0))
+    where dx = (b-a)/(d n)
+          dy = (b-a)/(d n)
 
 top_right :: Cij
-top_right a b n i j = (dx*(i),dy*(j))
-    where dx = (b-a)/(fromIntegral n)
-          dy = (b-a)/(fromIntegral n)
+top_right a b n i j = (dx*(d i),dy*(d j))
+    where dx = (b-a)/(d n)
+          dy = (b-a)/(d n)
 
 center :: Cij
 center a b n i j = (((fst bl)+(fst tr))/2, ((snd bl)+(snd tr))/2)
@@ -22,9 +25,9 @@ center a b n i j = (((fst bl)+(fst tr))/2, ((snd bl)+(snd tr))/2)
 
 reimman :: ((Double,Double) -> Double) -> Cij -> Integer -> Double -> Double -> Double
 reimman f cij n a b = sum single_sum
-    where indices = [(x,y) | x <- [1..(fromIntegral n)], y <- [1..(fromIntegral n)]]
+    where indices = [(x,y) | x <- [1..n], y <- [1..n]]
           cijf = cij a b n
-          dx = (b-a)/(fromIntegral n)
+          dx = (b-a)/(d n)
           single_sum = map (\((i,j)) -> (f $ cijf i j)*dx) indices
 
 
